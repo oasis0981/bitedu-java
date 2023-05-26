e1f211743dc3ec68afe62fb59a9f67622db7d62f
ys <hootingowl09@naver.com>
2023-05-26 20:54:19 +0900

    Feat: DTO, CM 생성

--- src/community/BoardDTO.java
+++ src/community/BoardDTO.java
@@ -1,4 +1,51 @@
 package community;
 
 public class BoardDTO {
+
+    private int bno;
+    private String btitle;
+    private String bcontent;
+    private String bwriter;
+    private String bdate;
+
+    public int getBno() {
+        return bno;
+    }
+
+    public String getBcontent() {
+        return bcontent;
+    }
+
+    public String getBdate() {
+        return bdate;
+    }
+
+    public String getBtitle() {
+        return btitle;
+    }
+
+    public String getBwriter() {
+        return bwriter;
+    }
+
+    public void setBno(int bno) {
+        this.bno = bno;
+    }
+
+    public void setBcontent(String bcontent) {
+        this.bcontent = bcontent;
+    }
+
+    public void setBdate(String bdate) {
+        this.bdate = bdate;
+    }
+
+    public void setBtitle(String btitle) {
+        this.btitle = btitle;
+    }
+
+    public void setBwriter(String bwriter) {
+        this.bwriter = bwriter;
+    }
+
 }
